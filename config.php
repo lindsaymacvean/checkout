eaf170c6d0e541d2eeef2f505752162eba594b3a
lindsay macvean <lindsaymacvean@gmail.com>
2018-01-08 03:31:12 +0000

    used getenv instead

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 require_once('vendor/autoload.php');
 
 $stripe = array(
-  "secret_key"      => $_ENV['secret_key'],
-  "publishable_key" => $_ENV['publishable_key']
+  "secret_key"      => getenv('secret_key'),
+  "publishable_key" => getenv('publishable_key')
 );
 
 \Stripe\Stripe::setApiKey($stripe['secret_key']);
