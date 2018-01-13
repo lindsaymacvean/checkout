d70e3ea1a68f04767b5755b2eea69ef0a8c61a88
lindsay macvean <lindsaymacvean@gmail.com>
2018-01-13 03:37:52 +0000

    something

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,10 @@ git add -A
 git commit -m "$1" 
 git push origin master
 
-if [[ $2 == "1" ]];
-  then
-    ssh ubuntu@longbackclothing.com << EOF
-      cd /var/www/html/misssexylegs
-      git pull origin master
-      sudo chown -R ubuntu:www-data .
-    EOF
+if [[ $2 == "1" ]]; then
+ssh ubuntu@longbackclothing.com << EOF
+  cd /var/www/html/misssexylegs
+  git pull origin master
+  sudo chown -R ubuntu:www-data .
+EOF
 fi
