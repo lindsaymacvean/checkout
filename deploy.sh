54e44aebbf04caca5ec8232955dc8d890b04e943
lindsay macvean <lindsaymacvean@gmail.com>
2018-01-13 03:10:10 +0000

    something

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+if [ $# -lt 1 ]
+  then
+    echo "You need a commit message"
+    exit 1
+fi
+
+git add -A
+git commit -m $1 
+git push origin master
+
+if [ $2 -eq 1 ]
+  then
+    #ssh ubuntu@longbackclothing.com << EOF
+    #  cd /var/www/html/missexy
+    #  git pull origin master
+    #  sudo chown -R ubuntu:www-data 
+    #EOF
+    echo "sm"
+fi
