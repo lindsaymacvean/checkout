8ec9c33907f4cd663b00e1e246bbd3b6ab49e641
lindsay macvean <lindsaymacvean@gmail.com>
2018-01-21 16:20:30 +0000

    added node server

---
+++ api/index.js
@@ -0,0 +1,17 @@
+// requiring the HTTP interfaces in node 
+var http = require('http'); 
+
+// create an http server to handle requests and response 
+http.createServer(function (req, res) { 
+	// sending a response header of 200 OK 
+	res.writeHead(200, 
+		{'Content-Type': 'text/plain'}
+	); 
+	// print out Hello World 
+	res.end('Hello World\n'); 
+	// use port 8080 
+}).listen(8080); 
+
+console.log('Server running on port 8080.');
+
+// Read more at https://www.pluralsight.com/guides/node-js/hello-world-node-js#LitdSu4Z11b1QSsS.99
